70b06448c2cf051ff904a985c2ab9bde0576001c
Christian Mollekopf <chrigi_1@fastmail.fm>
2017-08-25 15:58:46 -0600

    Add Q_DECL_OVERRIDE

--- autotests/kimap2test/fakeserver.h
+++ autotests/kimap2test/fakeserver.h
@@ -147,7 +147,7 @@ public:
      *
      * @reimp
      */
-    virtual void run();
+    virtual void run() Q_DECL_OVERRIDE;
 
     /**
      * Removes any previously-added scenarios, and adds a new one
