KDE/kimap

SearchJob: don't assemble OR term recursively
Summary:
The code was ridiculously inefficient for large OR sets. Trying
to assemble an OR term with 28'000 subterms took minutes and the
process eventually ran out of memory. Instead just use a for loop
and a bit of counting to balance the parentheses, the string
is assembled within milliseconds.

Test Plan:
Added a test to make sure the serialized string is the same as before

A benchmark for 10'000 subterms took 4 seconds. Benchmark for 28'000
subterms got killed by the OOM-killer. After the patch the benchmark
for 28'000 subterms took 2.2 ms.

Reviewers: #kde_pim, vkrause

Reviewed By: #kde_pim, vkrause

Subscribers: kde-pim

Tags: #kde_pim

Differential Revision: https://phabricator.kde.org/D28946

autotests/searchjobtest.cpp
@@ -164,6 +164,19 @@ private Q_SLOTS:
                      << "S: A000001 OK search done";
             QTest::newRow("uidbased NOT NEW search") << scenario << true << 6 << KIMAP::Term(KIMAP::Term::New).setNegated(true);
         }
+
+        {
+            QList<QByteArray> scenario;
+            scenario << FakeServer::preauth()
+                     << "C: A000001 UID SEARCH OR HEADER Message-Id \"<1234567@mail.box>\" (OR HEADER Message-Id \"<7654321@mail.box>\" (OR HEADER Message-Id \"<abcdefg@mail.box>\" HEADER Message-Id \"<gfedcba@mail.box>\"))"
+                     << "S: * SEARCH 1 2 3 4"
+                     << "S: A000001 OK search done";
+            KIMAP::Term term{KIMAP::Term::Or, {KIMAP::Term{QStringLiteral("Message-Id"), QStringLiteral("<1234567@mail.box>")},
+                                               KIMAP::Term{QStringLiteral("Message-Id"), QStringLiteral("<7654321@mail.box>")},
+                                               KIMAP::Term{QStringLiteral("Message-Id"), QStringLiteral("<abcdefg@mail.box>")},
+                                               KIMAP::Term{QStringLiteral("Message-Id"), QStringLiteral("<gfedcba@mail.box>")}}};
+            QTest::newRow("OR with multiple subterms") << scenario << true << 4 << term;
+        }
     }
 
     void testSearchTerm()

KDE/kimap2

autotests/searchjobtest.cpp
@@ -75,6 +75,19 @@ private Q_SLOTS:
                      << "S: A000001 OK search done";
             QTest::newRow("uidbased NOT NEW search") << scenario << true << 6 << KIMAP2::Term(KIMAP2::Term::New).setNegated(true);
         }
+
+        {
+            QList<QByteArray> scenario;
+            scenario << FakeServer::preauth()
+                     << "C: A000001 UID SEARCH OR HEADER Message-Id \"<1234567@mail.box>\" (OR HEADER Message-Id \"<7654321@mail.box>\" (OR HEADER Message-Id \"<abcdefg@mail.box>\" HEADER Message-Id \"<gfedcba@mail.box>\"))"
+                     << "S: * SEARCH 1 2 3 4"
+                     << "S: A000001 OK search done";
+            KIMAP2::Term term{KIMAP2::Term::Or, {KIMAP2::Term{QStringLiteral("Message-Id"), QStringLiteral("<1234567@mail.box>")},
+                                               KIMAP2::Term{QStringLiteral("Message-Id"), QStringLiteral("<7654321@mail.box>")},
+                                               KIMAP2::Term{QStringLiteral("Message-Id"), QStringLiteral("<abcdefg@mail.box>")},
+                                               KIMAP2::Term{QStringLiteral("Message-Id"), QStringLiteral("<gfedcba@mail.box>")}}};
+            QTest::newRow("OR with multiple subterms") << scenario << true << 4 << term;
+        }
     }
 
     void testSearchTerm()